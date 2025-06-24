3d328adf6ecd5ab35e5eb60b247d1bc8ea658303
Sue Smith <sue@benormal.info>
2025-06-24 21:43:36 +0000

    publish

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import handlebars from "vite-plugin-handlebars";
 import settings from "./codes.json";
+console.log(settings)
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         handlebars({
-            context: { settings }
+            context: settings 
         }),
     ],
     build: {
